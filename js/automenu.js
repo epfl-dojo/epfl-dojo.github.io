cbeeab3ebf54e90e05d56af1587d18d4dd318b4e
Richard Tenorio <richard.martinstenorio@epfl.ch>
2018-03-21 22:30:15 +0100

    Added smooth scroll + centered footer. Issues : when

--- js/automenu.js
+++ js/automenu.js
@@ -1,7 +1,7 @@
 // Parse all h{2-6} titles to create an automated menu
 // Have to be used with automenu.html
 $(function() {
-    $('h2, h3, h4, h5, h6').each(function() {
+    $('h1, h2, h3, h4, h5, h6').each(function() {
       $elf = $(this)
         $("#automenu_ul").append(
             $("<li>", {}).append(
